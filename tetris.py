1cc999ffcb2c9c6103a5d98474cc88035445c99d
Pedro Henrique <232038442@aluno.unb.br>
2025-01-08 18:51:28 -0300

    Update tetris.py

--- tetris.py
+++ tetris.py
@@ -15,21 +15,39 @@ class Text:
         return n_sin(time * 0.5), n_sin(time * 0.2), n_sin(time * 0.9)
 
     def draw(self):
+        # Texto de título (TETRIS no centro superior)
         title_text = 'TETRIS'
         title_pos = (WIN_W // 2, WIN_H * 0.05)
-        self.render_centered_text(title_text, title_pos, TILE_SIZE * 1.5, self.get_color())
+        self.render_centered_text(title_text, title_pos, TILE_SIZE * 1.2, self.get_color())
 
-        next_text = 'Next'
-        next_pos = (WIN_W * 0.8, WIN_H * 0.25)
-        self.render_centered_text(next_text, next_pos, TILE_SIZE * 1.0, 'orange')
-
+        # Pontuação atual
         score_label = 'Score'
-        score_label_pos = (WIN_W * 0.8, WIN_H * 0.6)
-        self.render_centered_text(score_label, score_label_pos, TILE_SIZE * 1.0, 'orange')
+        score_label_pos = (WIN_W * 0.8, WIN_H * 0.05)
+        self.render_centered_text(score_label, score_label_pos, TILE_SIZE * 0.6, 'orange')
 
         score_value = f'{self.app.tetris.score}'
-        score_value_pos = (WIN_W * 0.8, WIN_H * 0.7)
-        self.render_centered_text(score_value, score_value_pos, TILE_SIZE * 1.4, 'white')
+        score_value_pos = (WIN_W * 0.8, WIN_H * 0.1)
+        self.render_centered_text(score_value, score_value_pos, TILE_SIZE * 0.9, 'white')
+
+        # Maior pontuação (High Score)
+        high_score_label = 'High Score'
+        high_score_label_pos = (WIN_W * 0.8, WIN_H * 0.15)
+        self.render_centered_text(high_score_label, high_score_label_pos, TILE_SIZE * 0.6, 'orange')
+
+        high_score_value = self.get_high_score()
+        high_score_value_pos = (WIN_W * 0.8, WIN_H * 0.2)
+        self.render_centered_text(high_score_value, high_score_value_pos, TILE_SIZE * 0.9, 'white')
+
+        # Próximo Tetromino
+        next_text = 'Next'
+        next_pos = (WIN_W * 0.8, WIN_H * 0.3)
+        self.render_centered_text(next_text, next_pos, TILE_SIZE * 0.7, 'orange')
+
+    def get_high_score(self):
+        rankings = self.app.ranking_manager.rankings
+        if rankings:
+            return str(rankings[0]['score'])  # Maior score do ranking
+        return "0"
 
     def render_centered_text(self, text, position, size, color, bgcolor=None):
         text_surface, rect = self.font.render(text, fgcolor=color, size=size, bgcolor=bgcolor)
@@ -45,13 +63,14 @@ class Tetris:
         self.tetromino = Tetromino(self)
         self.next_tetromino = Tetromino(self, current=False)
         self.speed_up = False
-
         self.score = 0
         self.full_lines = 0
+        self.lines_cleared = 0
         self.points_per_lines = {0: 0, 1: 100, 2: 300, 3: 700, 4: 1500}
 
     def get_score(self):
         self.score += self.points_per_lines[self.full_lines]
+        self.lines_cleared += self.full_lines
         self.full_lines = 0
 
     def check_full_lines(self):
@@ -61,7 +80,7 @@ class Tetris:
                 self.field_array[row][x] = self.field_array[y][x]
 
                 if self.field_array[y][x]:
-                    self.field_array[row][x].pos = vec(x, y)
+                    self.field_array[row][x].pos = vec(x, row)
 
             if sum(map(bool, self.field_array[y])) < FIELD_W:
                 row -= 1
@@ -83,6 +102,8 @@ class Tetris:
     def is_game_over(self):
         if self.tetromino.blocks[0].pos.y == INIT_POS_OFFSET[1]:
             self.app.game_over = True
+            return True
+        return False
 
     def check_tetromino_landing(self):
         if self.tetromino.landing:
@@ -125,16 +146,4 @@ class Tetris:
     def draw(self):
         self.draw_grid()
         self.sprite_group.draw(self.app.screen)
-
-
-
-
-
-
-
-
-
-
-
-
-
+        
